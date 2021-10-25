5740f8ab402f7591eacdd16f9c5b454c965ff202
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2021-10-25 16:41:12 -0300

    exercícios fork aula 07/10

---
+++ exercicios-fork/exerc-fork-resp/fork-ex-1c.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+     pid_t f1, f2, f3, f4;
+     
+     printf("Processo principal (PID=%u)\n", getpid());
+     printf("A (PID=%u, pai=%u)\n", getpid(), getppid());
+     f1 = fork();
+     if (f1 == 0) {
+	  printf("B (PID=%u, pai=%u)\n", getpid(), getppid());
+     } else {
+	  f2 = fork();
+	  if (f2 == 0) {
+	       printf("C (PID=%u, pai=%u)\n", getpid(), getppid());
+	       f3 = fork();
+	       if (f3 == 0) {
+		    printf("D (PID=%u, pai=%u)\n", getpid(), getppid());
+	       } else {
+		    f4 = fork();
+		    if (f4 == 0) 
+			 printf("E (PID=%u, pai=%u)\n", getpid(), getppid());
+	       }
+	  }
+     }
+     sleep(60);
+     return 0;
+}
